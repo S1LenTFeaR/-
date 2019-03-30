f1cca45ed2997f870e013d5e55966e3e66472fe7
Andrey <killerus34@gmail.com>
2019-03-30 18:44:25 +0300

    Lab8 3

--- main.c
+++ main.c
@@ -1,23 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <time.h>
 int main()
 {
-int C=0, i, N=0, Arr[50];
- srand(time(NULL));
-for(i=0;i<50;i++)
+int C=8, O=0, o=0, i, Arr[9];
+srand(time(NULL));
+for(i=0;i<9;i++)
 {
-    Arr[i]=(2+rand()%4);
+    Arr[i]=rand()%5;
     printf("%d ",Arr[i]);
-    if(Arr[i]>2)
+}
+
+for(i=0;i<9;i++)
+{
+    if(Arr[i]==0)
     {
+        o++;
     }
     else
     {
-        N+=1;
     }
 }
-printf("\nN=%d",N);
+for(i=0;i<(9-o);i++)
+{
+    if(Arr[i]==0)
+    {
+        if(Arr[C-O]!=0)
+        {
+            Arr[i]=Arr[C-O];
+            Arr[C-O]=0;
+            O++;
+        }
+        else
+        {
+            O++;
+            Arr[i]=Arr[C-O];
+            Arr[C-O]=0;
+        }
+    }
+    else
+    {
+
+    }
+}
+    printf("\n");
+for(i=0;i<9;i++)
+{
+    printf("%d ",Arr[i]);
+}
 return 0;
 }
