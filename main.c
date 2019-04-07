495e0a8e5821890926bafe2dc8ea699ecdbb9e77
Andrey <killerus34@gmail.com>
2019-04-07 17:10:02 +0300

    Lab9 1

--- main.c
+++ main.c
@@ -1,53 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <time.h>
 int main()
 {
-int C, O=0, o=0, i, N=10, Arr[N];
-srand(time(NULL));
-for(i=0;i<N;i++)
-{
-    Arr[i]=rand()%5;
-    printf("%d ",Arr[i]);
-}
-
-for(i=0;i<N;i++)
-{
-    if(Arr[i]==0)
-    {
-        o++;
-    }
-    else
-    {
-    }
-}
-C=i-1;
-for(i=0;i<(N-o);i++)
-{
-    if(Arr[i]==0)
-    {
-        if(Arr[C-O]!=0)
-        {
-            Arr[i]=Arr[C-O];
-            Arr[C-O]=0;
-            O++;
-        }
-        else
-        {
-            O++;
-            i--;
-        }
-    }
-    else
-    {
-
-    }
-}
-    printf("\n");
-for(i=0;i<N;i++)
-{
-    printf("%d ",Arr[i]);
-}
-return 0;
+    int I,*i,Size;
+    short S,*s;
+    char C,*c;
+    long L,*l;
+    float F,*f;
+    double D,*d;
+    long double Ld,*ld;
+    i=&I;
+    s=&S;
+    c=&C;
+    l=&L;
+    f=&F;
+    d=&D;
+    ld=&Ld;
+    Size=sizeof(I);
+    printf("Size int = %d\n\n",Size);
+    Size=sizeof(*i);
+    printf("Size *int = %d\n\n",Size);
+    Size=sizeof(S);
+    printf("Size short = %d\n\n",Size);
+    Size=sizeof(*s);
+    printf("Size *short = %d\n\n",Size);
+    Size=sizeof(C);
+    printf("Size char = %d\n\n",Size);
+    Size=sizeof(*c);
+    printf("Size *char = %d\n\n",Size);
+    Size=sizeof(L);
+    printf("Size long = %d\n\n",Size);
+    Size=sizeof(l);
+    printf("Size *long = %d\n\n",Size);
+    Size=sizeof(F);
+    printf("Size float = %d\n\n",Size);
+    Size=sizeof(*f);
+    printf("Size *float = %d\n\n",Size);
+    Size=sizeof(D);
+    printf("Size double = %d\n\n",Size);
+    Size=sizeof(*d);
+    printf("Size *double = %d\n\n",Size);
+    Size=sizeof(Ld);
+    printf("Size long double = %d\n\n",Size);
+    Size=sizeof(*ld);
+    printf("Size *long double = %d\n\n",Size);
+    return 0;
 }
