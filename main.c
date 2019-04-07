ac84e01fbb4a9718ca26b34072bec966b6c08122
Andrey <killerus34@gmail.com>
2019-04-07 17:11:15 +0300

    Lab9 2

--- main.c
+++ main.c
@@ -1,49 +1,26 @@
+/*Программа №2 с лабораторной работы №8*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+int SUM(int *arr)
+{
+    int i=0, Sum=0;
+    while (arr[i] <= 0)
+    {
+        i++;
+    }
+    i++;
+    while (arr[i] <= 0)
+    {
+        Sum += arr[i];
+        i++;
+    }
+    return Sum;
+}
 int main()
 {
-    int I,*i,Size;
-    short S,*s;
-    char C,*c;
-    long L,*l;
-    float F,*f;
-    double D,*d;
-    long double Ld,*ld;
-    i=&I;
-    s=&S;
-    c=&C;
-    l=&L;
-    f=&F;
-    d=&D;
-    ld=&Ld;
-    Size=sizeof(I);
-    printf("Size int = %d\n\n",Size);
-    Size=sizeof(*i);
-    printf("Size *int = %d\n\n",Size);
-    Size=sizeof(S);
-    printf("Size short = %d\n\n",Size);
-    Size=sizeof(*s);
-    printf("Size *short = %d\n\n",Size);
-    Size=sizeof(C);
-    printf("Size char = %d\n\n",Size);
-    Size=sizeof(*c);
-    printf("Size *char = %d\n\n",Size);
-    Size=sizeof(L);
-    printf("Size long = %d\n\n",Size);
-    Size=sizeof(l);
-    printf("Size *long = %d\n\n",Size);
-    Size=sizeof(F);
-    printf("Size float = %d\n\n",Size);
-    Size=sizeof(*f);
-    printf("Size *float = %d\n\n",Size);
-    Size=sizeof(D);
-    printf("Size double = %d\n\n",Size);
-    Size=sizeof(*d);
-    printf("Size *double = %d\n\n",Size);
-    Size=sizeof(Ld);
-    printf("Size long double = %d\n\n",Size);
-    Size=sizeof(*ld);
-    printf("Size *long double = %d\n\n",Size);
-    return 0;
+int Sum, Arr[20]={-5, 6, -8, -25, -35, -47, 12, 34, 78, -5987, 11, 12, 13, 14, 15, 16, 17, 18, -19, 20}, *arr;
+arr=&Arr;
+printf("\nSUM=%d\n",SUM(&Arr));
+return 0;
 }
