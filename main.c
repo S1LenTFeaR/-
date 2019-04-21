abc963ed4bea30361bc5063737cc6e6fb1b63d30
Andrey <killerus34@gmail.com>
2019-04-21 23:38:09 +0300

    Lab10 2

--- main.c
+++ main.c
@@ -1,30 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+#include <time.h>
+#include <locale.h>
+#define N 6
+#define M 6
 int main()
 {
-    int N=0, i=0;
-    char *A, *B, C, Arr[]="If we cannot do as we would we";
-    N = strlen(Arr);
-    N--;
-    A=&Arr[0];
-    B=&Arr[N];
-    printf("%s\n", Arr);
+    setlocale(LC_ALL, "Rus");
+    int i, j, C=0, sum=0, SUM=0;
+    int Arr[N][M];
     for(i=0;i<N;i++)
     {
-        if(i<N-i)
+        printf("Строка %d|", i+1);
+        for(j=0;j<M;j++)
         {
-            C=*A;
-            *A=*B;
-            *B=C;
-            A++;
-            B--;
+            Arr[i][j]=rand()%2;
+            printf("%4d",Arr[i][j]);
         }
-        else
-        {
-            i++;
-        }
+        printf("\n");
     }
-    printf("%s\n", Arr);
+    C+=Arr[0][0];
+    for (i = 0; i<N-1; i++)
+    {
+        sum=0;
+        for (j = 0; j<M; j++)
+        {
+            C = Arr[i][j]*Arr[i+1][j];
+            sum+=abs(C);
+        }
+        SUM+=sum;
+        printf("\nСкалярное произведение  %d и %d строк по модулю = %d", i+1, i+2, sum);
+    }
+    printf("\n");
+    printf("\nСумма скалярных произведений по модулю = %d\n", SUM);
+    if(SUM==0)
+        printf("\nМатрица - ортонормированная");
+    else
+        printf("\nМатрица - Неортонормированная\n");
     return 0;
 }
