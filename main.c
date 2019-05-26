efc43baac3dcc4aab8f4e744e689cea48fc49ccc
Andrey <killerus34@gmail.com>
2019-05-26 13:36:59 +0300

    lab11 1.2

--- main.c
+++ main.c
@@ -1,91 +1,107 @@
 #include<stdio.h>
-#include <stdlib.h>
-#define i 5
-struct NOTE {
-    char *Second_Name, *First_Name;
+#include<stdlib.h>
+#include<locale.h>
+#define N 3 //Максимальное количество участников
+#define YEAR 1990 //Год, с которого начинается сортировка
+struct NOTE
+{
+    char First_Name[9], Second_Name[9];
     int Number;
-    int *date[3];
-} arr[i];
+    int date[3];
+    int Date
+} arr[N], tmp;
 
 int main()
 {
-    int k, j=0, l=0, Arr[5], Date=1990;
-    arr[0].Second_Name = "Gorbach";
-    arr[0].First_Name = "Andrey";
-    arr[0].date[0] = 1;
-    arr[0].date[1] = 2;
-    arr[0].date[2] = 2000;
+    setlocale(LC_ALL, "Rus");
 
-    arr[1].Second_Name = "Sidorov";
-    arr[1].First_Name = "Ivan";
-    arr[1].date[0] = 3;
-    arr[1].date[1] = 1;
-    arr[1].date[2] = 2003;
+    int k, n, j = 0;
 
-    arr[2].Second_Name = "Ivanov";
-    arr[2].First_Name = "Alexey";
-    arr[2].date[0] = 30;
-    arr[2].date[1] = 11;
-    arr[2].date[2] = 1998;
 
-    arr[3].Second_Name = "Andreev";
-    arr[3].First_Name = "Andrey";
-    arr[3].date[0] = 25;
-    arr[3].date[1] = 9;
-    arr[3].date[2] = 2000;
-
-    arr[4].Second_Name = "Vasiliev";
-    arr[4].First_Name = "Evgeniy";
-    arr[4].date[0] = 7;
-    arr[4].date[1] = 5;
-    arr[4].date[2] = 1990;
-    for(k = 0; k < i; k++)
+    for(k = 0 ; k < N ; k++)
     {
-        printf("Enter mobile number by %s %s\n", arr[k].First_Name, arr[k].Second_Name);
-        scanf("%int", &arr[k].Number);
+        printf("Участник %d:\n",k+1);
+        printf("Введите имя: ");
+        scanf("%8s",arr[k].First_Name);
+        printf("Введите фамилию: ");
+        scanf("%8s",arr[k].Second_Name);
+        printf("Введите номер телефона: ");
+        scanf("%8d",&arr[k].Number);
+        printf("Введите день рождения: ");
+        scanf("%8d",&arr[k].date[0]);
+        printf("Введите неделю рождения: ");
+        scanf("%8d",&arr[k].date[1]);
+        printf("Введите год рождения: ");
+        scanf("%8d",&arr[k].date[2]);
     }
-    printf("---------------------------------------------\n");
-        for(k = 0; k < 100; k++)
+
+    //Подсчет количества дней после года YEAR для каждого участника анкеты
+    for(k = 0 ; k < N ; k++)
+    {
+        arr[k].Date = (arr[k].date[0]) + (arr[k].date[1]*30) + ((arr[k].date[2]-YEAR)*360);
+    }
+    //Счетчик итераций (сортировка на протяжении n дней после года YEAR)
+    for(n = 0; n < 50000 ; n++)
+    {
+        //Проверка каждого человека (родился ли человекв n-ный день)
+        for(k = 0 ; k < N ; k++)
         {
-            if(arr[l].date[2] == Date)
+            if(arr[k].Date == n)
             {
-                while(l<i)
-                {
-                    if(arr[l].date[2] == Date)
-                    {
-                        printf("First_Name - %s, Second_Name - %s\n", arr[l].First_Name, arr[l].Second_Name);
-                        if(arr[l].Number!=0)
-                        {
-                            printf("Number = %d\n", arr[l].Number);
-                        }
-                        else
-                        printf("Unknown number\n");
-                        printf("date = %d,%d,%d\n", arr[l].date[0], arr[l].date[1], arr[l].date[2]);
-                        printf("---------------------------------------------\n");
-                        l++;
-                    }
-                    else
-                    {
-                        l++;
-                    }
-                }
-                Date++;
-                l=0;
+                //Делаем копию заменяемого элемента массива структуры
+                strcpy(tmp.First_Name,arr[j].First_Name );
+                strcpy(tmp.Second_Name,arr[j].Second_Name);
+                tmp.Number = arr[j].Number;
+                tmp.date[0] = arr[j].date[0];
+                tmp.date[1] = arr[j].date[1];
+                tmp.date[2] = arr[j].date[2];
+                tmp.Date = arr[j].Date;
+                //Заменяем элемент массива структуры
+                strcpy(arr[j].First_Name , arr[k].First_Name);
+                strcpy(arr[j].Second_Name , arr[k].Second_Name);
+                arr[j].Number = arr[k].Number;
+                arr[j].date[0] = arr[k].date[0];
+                arr[j].date[1] = arr[k].date[1];
+                arr[j].date[2] = arr[k].date[2];
+                arr[j].Date = arr[k].Date;
+                j++;
+                //Возвращаем значение замененного элемента массива структуры на место k
+                strcpy(arr[k].First_Name , tmp.First_Name);
+                strcpy(arr[k].Second_Name , tmp.Second_Name);
+                arr[k].Number=tmp.Number;
+                arr[k].date[0]=tmp.date[0];
+                arr[k].date[1]=tmp.date[1];
+                arr[k].date[2]=tmp.date[2];
+
+                arr[k].Date=tmp.Date;
             }
             else
             {
-                if(l<i)
-                {
-                    l++;
-                }
-                else
-                {
-                    Date++;
-                    l=0;
-                }
             }
+
         }
-        return 0;
+
+    }
+
+    printf("\n---------------------------------------------\n");
+    //Вывод отсортированной по датам анкеты
+    for(k = 0 ; k < N ; k++)
+    {
+        printf("Имя - %s, Фамилия - %s\n", arr[k].First_Name, arr[k].Second_Name);
+        //Проверка, был ли введен номер телефона (Если 0 -нет, если иное число - телефон введен)
+        if(arr[k].Number!=0)
+        {
+            printf("Номер телефона: %d\n", arr[k].Number);
+        }
+        else
+        {
+            printf("Номер телефона не указан\n");
+        }
+        printf("Дата рождения: %d.%d.%d\n", arr[k].date[0], arr[k].date[1], arr[k].date[2]);
+        printf("---------------------------------------------\n");
+    }
+
+    return 0;
 }
 
 
